adfaf888c82ff165f3f4e607cde90e4f5e24ebac
Timothy Zatloff <timzatloff@gmail.com>
2025-11-12 20:49:11 -0500

    PYCOR-23: Developed prompts for user data entry and processing of entered values

---
+++ Python-Course/task_1.py
@@ -0,0 +1,10 @@
+# Create input prompts for the user to enter the number of lemons, sugar per cup, and base price.
+# Store these values in variables using the input() function and ensure correct data types are used.
+
+lemons = int(input("Enter thr number of lemons: "))
+
+sugar_per_cup = float(input("Enter the amount of sugar per cup (in grams): "))
+
+base_price = float(input("Enter the base price: "))
+
+print(f"Lemons: {lemons}\nSugar per cup: {sugar_per_cup}\nBase price: {base_price}\n")
