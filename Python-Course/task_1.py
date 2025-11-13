35ecab2ba5ee4657cf91f40638e3f3ed392f8e4e
Timothy Zatloff <timzatloff@gmail.com>
2025-11-12 21:45:15 -0500

    PYCOR-23:Added conditional warning for low lemons

--- Python-Course/task_1.py
+++ Python-Course/task_1.py
@@ -2,6 +2,7 @@
 # Store these values in variables using the input() function and ensure correct data types are used.
 # Write code to calculate the total number of cups possible and the estimated revenue based on user input.
 # Use arithmetic operators and variables to perform these calculations.
+# Implement an if statement to check if the number of lemons is less than 5. If true, print a warning message to the user.
 
 lemons = int(input("Enter thr number of lemons: "))
 
@@ -11,6 +12,11 @@ base_price = float(input("Enter the base price: "))
 
 total_cups = lemons
 revenue = total_cups * base_price
+
+if lemons < 5:
+    print("Warning: You have less than 5 lemons!")
+else:
+    print("You have plenty of lemons!")
 
 print(f"Lemons: {lemons}\nSugar per cup: {sugar_per_cup}\nBase price: {base_price}\n")
 print("Cups:", total_cups)
