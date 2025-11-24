66e76e186383b79e0382e000cbe4d6bcc7ae0414
Timothy Zatloff <timzatloff@gmail.com>
2025-11-23 21:21:21 -0500

    PYCOR-41 Prompt user for slice indices, and handles potential invalid or negative inputs

--- Python-Course/week 9/playlist_slicer.py
+++ Python-Course/week 9/playlist_slicer.py
@@ -19,3 +19,19 @@ playlist = [
     "Kendrick Lamar - HUMBLE.",
     "Post Malone - Rockstar"
 ]
+print(playlist[1:4])
+print(playlist[-4:-1])
+print(playlist[::-1])
+print("Post Malone - Rockstar" in playlist)
+
+# PYCOR-42: Prompt user for slice indices and handle input safely
+# Prompts the user for start, end, and step values, converts them to integers,
+# and handles potential invalid or negative inputs.
+try:
+    start = int(input("Enter the index (can negative, e.g, -3): "))
+    end = int(input("Enter end index (exclusive, can be negative, e.g, -1): "))
+    step = int(input("Enter step (e.g, 1 for normal, -1 for reverse): "))
+
+    print(f"\nYou entered: start={start}, end={end}, step={step}")
+except ValueError:
+    print("Invalid input! Please enter integer values only.")
